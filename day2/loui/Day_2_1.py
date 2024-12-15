0cda0e41b9accccf3586a6078849d7abe42494ed
Loui <69811481+Sh4yen@users.noreply.github.com>
2024-12-15 17:44:18 +0100

    Deleted wrong file in last commit. Restored changes and deleted correct file. Day 2 part 1 complete

--- day2/loui/Day_2_1.py
+++ day2/loui/Day_2_1.py
@@ -1,16 +1,47 @@
 import numpy as np
 import scipy.ndimage as sp
 
-data = np.array([[3, 2, 1], [4, 5, 6], [7, 8, 6]])
-a_data =[row for row in data if np.array_equal(row, np.sort(row)[::-1]) or np.array_equal(row, np.sort(row))]
-print(a_data)
+# lies die Input datei ein
+def read_input_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        # Jede Zeile im file wird eine Liste von Zahlen
+        # Die Listen werden horizontal in eine Matrix gespeichert
+        # Jdede Zeile entspricht einer Zeile im file
+        # Jede Spalte entspricht einer Zahl in der Zeile
+        data = [list(map(int, line.split(" "))) for line in lines]
+    return data
+
+data = read_input_file('input.txt')
+
+# Behalte nur die aufsteigenden und absteigenden Zeilen, enthält auch zeilen wie [1, 2, 2, 3] diese dopplungen müssen 
+# noch entfernt werden
+# [::-1] dreht die Liste um, np.sort(row)[::-1] ist also die absteigende Reihenfolge
+rise_fall_data = [row for row in data if np.array_equal(row, np.sort(row)) or np.array_equal(row, np.sort(row)[::-1])]
+# Entferne alle Zeilen die dopplungen enthalten, eine Zeile wird nur beibehalten, wenn sie genau so lang ist wie die 
+# Menge ihrer einzigartigen Elemente
+unique_rise_fall_data = [row for row in rise_fall_data if len(row) == len(set(row))]
+
+
+
+
 gewichtung = np.array([-1, 1])
+# Horizontale sprunggröße zwischen allen elementen der matrix berechnen
+# Faltung (convolution) jeder Zeile von unique_rise_fall_data mit dem Vektor [-1, 1], bestimmt sprunggröße zwischen 
+# benachbarten elementen
+# bsp. x = convolve1d([2, 1, 3], [-1, 1], mode="wrap") -> x = [(-2+1), (-2+3), (-3+2)] = [-1, 2, -1]
+jump_data = [sp.convolve1d(row, gewichtung, mode="wrap") for row in unique_rise_fall_data]
+# Es intressiert nur die sprunggröße, nicht die Richtung, daher wird der Betrag genommen
+# Die letzte Spalte wird entfernt, da sie nur die Sprunggröße zwischen dem letzten und ersten Element der Zeile enthält
+# diese ist nicht relevant
+cut_abs_jump_data = [abs(row[:-1]) for row in jump_data]
 
-# ascending_data = np.array([sp.convolve1d(row, gewichtung, mode='wrap') for row in ascending_data])
-a_data = np.array(sp.convolve1d(a_data, gewichtung, mode='wrap'))
-# descending_data_data = np.array([sp.convolve1d(row, gewichtung, mode='wrap') for row in descending_data])
-a_data = abs(data[:, :-1])
+# Behalte Nur Zeilen bei denen die Sprunggröße zwischen benachbarten Elementen in 3 oder kleiner ist
+safe_cut_abs_jump_data = [row for row in cut_abs_jump_data if all(jump <= 3 for jump in row)]
 
+# Die länge dieser Liste ist die Menge aller sicheren Regeln
+# Das prüfen nach Dopplungen hätte auch durch eine jump größe von min 1 erledigt werden können
+print(safe_cut_abs_jump_data)
+print("_____________________________________________________________________________________________\n")
+print(len(safe_cut_abs_jump_data)," Reports are sasfe")
 
-print(a_data)
-print(np.vstack((a_data, gewichtung)))
